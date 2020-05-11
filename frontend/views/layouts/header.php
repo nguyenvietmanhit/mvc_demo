599c97190e33ddb0c579fb534ba0033de289cec2
Nguyen Viet Manh <nvmanh@cmc.com.vn>
2020-05-11 15:14:21 +0700

    update

--- frontend/views/layouts/header.php
+++ frontend/views/layouts/header.php
@@ -1,158 +1,172 @@
-<?php
-$year = '';
-$username = '';
-$jobs = '';
-$avatar = '';
-if (isset($_SESSION['user'])) {
-    $username = $_SESSION['user']['username'];
-    $jobs = $_SESSION['user']['jobs'];
-    $avatar = $_SESSION['user']['avatar'];
-    $year = date('Y', strtotime($_SESSION['user']['created_at']));
-}
+<!-- header start -->
+<header class="header">
+    <div class="header-wrapper">
 
-?>
-<header class="main-header">
-    <!-- Logo -->
-    <a href="index2.html" class="logo">
-        <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini"><b>A</b>LT</span>
-        <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><b>Admin</b>LTE</span>
-    </a>
-    <!-- Header Navbar: style can be found in header.less -->
-    <nav class="navbar navbar-static-top">
-        <!-- Sidebar toggle button-->
-        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
-            <i class="fa fa-bars"></i>
-        </a>
+        <!--sidebar menu toggler start -->
+        <div class="toggle-sidebar material-button">
+            <i class="material-icons">&#xE5D2;</i>
+        </div>
+        <!--sidebar menu toggler end -->
 
-        <div class="navbar-custom-menu">
-            <ul class="nav navbar-nav">
-                <!-- User Account: style can be found in dropdown.less -->
-                <li class="dropdown user user-menu">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <img src="assets/uploads/<?php echo $avatar; ?>" class="user-image" alt="User Image">
-                        <span class="hidden-xs"><?php echo $username; ?></span>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <!-- User image -->
-                        <li class="user-header">
-                            <img src="assets/uploads/<?php echo $avatar; ?>" class="img-circle" alt="User Image">
+        <!--logo start -->
+        <div class="logo-box">
+            <h1>
+                <a href="index.html" class="logo"></a>
+            </h1>
+        </div>
+        <!--logo end -->
 
-                            <p>
-                                <?php echo $username . ' - ' . $jobs; ?>
-                                <!--Nguyễn Viết Mạnh - Web Developer-->
-                                <small>Thành viên từ năm <?php echo $year; ?></small>
-                            </p>
-                        </li>
-                        <!-- Menu Footer-->
-                        <li class="user-footer">
-                            <div class="pull-left">
-                                <a href="#" class="btn btn-default btn-flat">Profile</a>
-                            </div>
-                            <div class="pull-right">
-                                <a href="index.php?controller=user&action=logout" class="btn btn-default btn-flat">Sign
-                                    out</a>
-                            </div>
-                        </li>
-                    </ul>
+        <div class="header-menu">
+
+            <!-- header left menu start -->
+            <ul class="header-navigation" data-show-menu-on-mobile>
+                <li>
+                    <a href="#" class="material-button submenu-toggle">HOME</a>
+
                 </li>
+                <li>
+                    <a href="#" class="material-button submenu-toggle">
+                        Thế giới &nbsp;
+                        <span class="fa fa-angle-down"></span>
+                    </a>
+                    <div class="header-submenu">
+                        <ul>
+                            <li><a href="index.html">Điểm tin thế giới</a></li>
+                            <li><a href="index2.html">Khám phá</a></li>
+                            <li><a href="index3.html">Hồ sơ</a></li>
+                            <li><a href="index4.html">Người Việt năm châu</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <a href="#" class="material-button submenu-toggle">Xã hội</a>
+                </li>
+                <li>
+                    <a href="#" class="material-button submenu-toggle">Kinh doanh</a>
+                </li>
+                <li>
+                    <a href="#" class="material-button submenu-toggle">Sức khoẻ</a>
+                </li>
+                <li>
+                    <a href="#" class="material-button submenu-toggle">Entertaiment</a>
+                </li>
+                <li>
+                    <a href="#" class="material-button submenu-toggle">Công nghệ</a>
+                </li>
+                <li>
+                    <a href="#" class="material-button submenu-toggle">Đời sống</a>
+                </li>
+                <li>
+                    <a href="#" class="material-button submenu-toggle">Du lịch</a>
+                </li>
+                <li>
+                    <a href="#" class="material-button submenu-toggle">Thể thao</a>
+                </li>
+                <li>
+                    <a href="#" class="material-button submenu-toggle">Videos</a>
+                </li>
+                <li>
+                    <a href="#" class="material-button submenu-toggle">Xe cộ</a>
+                </li>
+                <!--<li>-->
+                <!--<a href="#" class="material-button submenu-toggle">Search Job</a>-->
+                <!--</li>-->
+                <!--<li>-->
+                <!--<a href="#" class="material-button submenu-toggle">Dating</a>-->
+                <!--</li>-->
+                <!--<li>-->
+                <!--<a href="#" class="material-button submenu-toggle">Ẩm thực</a>-->
+                <!--</li>-->
+                <!--<li>-->
+                <!--<a href="#" class="material-button submenu-toggle">Giáo dục</a>-->
+                <!--</li>-->
+                <!--<li>-->
+                <!--<a href="#" class="material-button submenu-toggle">POSTS</a>-->
+                <!--<div class="header-submenu">-->
+                <!--<ul>-->
+                <!--<li><a href="list-timeline.html">List timeline</a></li>-->
+                <!--<li><a href="list-two-column.html">List column 2</a></li>-->
+                <!--<li><a href="list-three-column.html">List column 3</a></li>-->
+                <!--<li><a href="detail-standart.html">Detail standart</a></li>-->
+                <!--<li><a href="detail-parallax.html">Detail parallax</a></li>-->
+                <!--<li><a href="detail-with-large-adbox.html">Detail adbox 1</a></li>-->
+                <!--<li><a href="detail-with-slim-adbox.html">Detail adbox 2</a></li>-->
+                <!--<li><a href="detail-left-sidebar.html">Left sidebar</a></li>-->
+                <!--<li><a href="detail-left-sidebar-adbox.html">Left sidebar adbox</a></li>-->
+                <!--<li><a href="detail-full-width.html">Full width</a></li>-->
+                <!--</ul>-->
+                <!--</div>-->
+                <!--</li>-->
+                <!--<li>-->
+                <!--<a href="#" class="material-button submenu-toggle">VIDEO</a>-->
+                <!--<div class="header-submenu">-->
+                <!--<ul>-->
+                <!--<li><a href="video-standart.html">Video standart</a></li>-->
+                <!--<li><a href="video-iframe.html">Video iframe</a></li>-->
+                <!--<li><a href="video-custom-player.html">Video custom player</a></li>-->
+                <!--</ul>-->
+                <!--</div>-->
+                <!--</li>-->
+                <!--<li>-->
+                <!--<a href="#" class="material-button submenu-toggle">EXTRA PAGES <i-->
+                <!--class="material-icons">&#xE313;</i></a>-->
+                <!--<div class="header-submenu">-->
+                <!--<ul>-->
+                <!--<li><a href="authors.html">Authors</a></li>-->
+                <!--<li><a href="author-posts-1.html">Author posts-column</a></li>-->
+                <!--<li><a href="author-posts-2.html">Author posts-timeline</a></li>-->
+                <!--<li><a href="about-us.html">About us</a></li>-->
+                <!--<li><a href="privacy-policy.html">Privacy policy</a></li>-->
+                <!--<li><a href="contact.html">Contact</a></li>-->
+                <!--<li><a href="error.html">Error</a></li>-->
+                <!--</ul>-->
+                <!--</div>-->
+                <!--</li>-->
             </ul>
+            <!-- header left menu end -->
+
         </div>
-    </nav>
+        <div class="header-right with-seperator">
+
+            <!-- header right menu start -->
+            <ul class="header-navigation">
+                <li>
+                    <a href="#" class="material-button search-toggle"><i class="material-icons">&#xE8B6;</i></a>
+                </li>
+                <li>
+                    <a href="#" class="material-button submenu-toggle"><i class="material-icons">&#xE7FD;</i> <span
+                                class="hide-on-tablet">Tài khoản</span></a>
+                    <div class="header-submenu">
+                        <ul>
+                            <li><a href="#" data-modal="loginModal">Đăng nhập</a></li>
+                            <li><a href="#" data-modal="registerModal">Đăng ký</a></li>
+                            <li><a href="profile.html">Profile</a></li>
+                            <li><a href="#">Logout</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <!--<li class="hide-on-mobile"><a href="#" class="material-button" data-modal="newsletterModal"><i-->
+                <!--class="material-icons">&#xE0E1;</i></a></li>-->
+            </ul>
+            <!-- header right menu end -->
+
+        </div>
+
+        <!--header search panel start -->
+        <div class="search-bar">
+            <form class="search-form">
+                <div class="search-input-wrapper">
+                    <input type="text" name="qq" placeholder="Tìm kiếm..." class="search-input">
+                    <button type="submit" name="search" class="search-submit"><i class="material-icons">&#xE5C8;</i>
+                    </button>
+                </div>
+                <span class="search-close search-toggle">
+						<i class="material-icons">&#xE5CD;</i>
+					</span>
+            </form>
+        </div>
+        <!--header search panel end -->
+
+    </div>
 </header>
-<!-- Left side column. contains the logo and sidebar -->
-<aside class="main-sidebar">
-    <!-- sidebar: style can be found in sidebar.less -->
-    <section class="sidebar">
-        <!-- Sidebar user panel -->
-        <div class="user-panel">
-            <div class="pull-left image">
-                <img src="assets/uploads/<?php echo $avatar; ?>" class="img-circle" alt="User Image">
-            </div>
-            <div class="pull-left info">
-                <p><?php echo $username; ?></p>
-                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-            </div>
-        </div>
-        <!-- sidebar menu: : style can be found in sidebar.less -->
-        <ul class="sidebar-menu" data-widget="tree">
-            <li class="header">LAOYOUT ADMIN</li>
-
-            <li>
-                <a href="index.php?controller=category&action=index">
-                    <i class="fa fa-th"></i> <span>Quản lý danh mục</span>
-                    <span class="pull-right-container">
-              <!--<small class="label pull-right bg-green">new</small>-->
-            </span>
-                </a>
-            </li>
-            <li>
-                <a href="index.php?controller=product&action=index">
-                    <i class="fa fa-code"></i> <span>Quản lý sản phẩm</span>
-                    <span class="pull-right-container">
-              <!--<small class="label pull-right bg-green">new</small>-->
-            </span>
-                </a>
-            </li>
-            <li>
-                <a href="index.php?controller=user&action=index">
-                    <i class="fa fa-user"></i> <span>Quản lý user</span>
-                    <span class="pull-right-container">
-              <!--<small class="label pull-right bg-green">new</small>-->
-            </span>
-                </a>
-            </li>
-        </ul>
-    </section>
-    <!-- /.sidebar -->
-</aside>
-
-<!-- Breadcrumd Wrapper. Contains breadcrumb -->
-<div class="breadcrumb-wrap content-wrap content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-        <h1>
-            Dashboard
-            <small>Control panel</small>
-        </h1>
-        <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li class="active">Dashboard</li>
-        </ol>
-    </section>
-</div>
-
-<!-- Messaeg Wrapper. Contains messaege error and success -->
-<div class="message-wrap content-wrap content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-        <?php if (isset($_SESSION['error'])): ?>
-            <div class="alert alert-danger">
-                <?php
-                echo $_SESSION['error'];
-                unset($_SESSION['error']);
-                ?>
-            </div>
-        <?php endif; ?>
-
-        <?php if (!empty($this->error)): ?>
-            <div class="alert alert-danger">
-                <?php
-                echo $this->error;
-                ?>
-            </div>
-        <?php endif; ?>
-
-        <?php if (isset($_SESSION['success'])): ?>
-            <div class="alert alert-success">
-                <?php
-                echo $_SESSION['success'];
-                unset($_SESSION['success']);
-                ?>
-            </div>
-        <?php endif; ?>
-        <!--        <div class="alert alert-danger">Lỗi validate</div>-->
-        <!--        <p class="alert alert-success">Thành công</p>-->
-    </section>
-</div>
+<!-- header end -->
