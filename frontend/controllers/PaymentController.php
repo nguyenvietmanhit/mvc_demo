d879764cfa38f9614c213371676186f0d09b508d
Manh <nvmanh@cmc.com.vn>
2020-06-09 07:38:59 +0700

    update

--- frontend/controllers/PaymentController.php
+++ frontend/controllers/PaymentController.php
@@ -2,6 +2,9 @@
 require_once 'controllers/Controller.php';
 require_once 'models/Order.php';
 require_once 'models/OrderDetail.php';
+require_once 'configs/PHPMailer/src/PHPMailer.php';
+require_once 'configs/PHPMailer/src/SMTP.php';
+require_once 'configs/PHPMailer/src/Exception.php';
 
 class PaymentController extends Controller {
   public function index() {
@@ -54,7 +57,10 @@ class PaymentController extends Controller {
 //          unset($_SESSION['cart']);
           $_SESSION['order'] = $order;
           $_SESSION['success'] = 'Lưu thông tin thanh toán thành công';
-          header("Location: phuong-thuc-thanh-toan");
+          //gửi mail
+          $this->sendMail($email);
+          die;
+          header("Location: cam-on");
           exit();
         } else {
           $_SESSION['error'] = 'Lưu thông tin thanh toán thất bại';
@@ -80,5 +86,53 @@ class PaymentController extends Controller {
     $this->content = $this->render('configs/nganluong/index.php');
 
     require_once 'views/layouts/main.php';
+  }
+
+  protected function sendMail($email) {
+    // Instantiation and passing `true` enables exceptions
+    $mail = new \PHPMailer\PHPMailer\PHPMailer(true);
+
+    try {
+      //Server settings
+      $mail->SMTPDebug = \PHPMailer\PHPMailer\SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+      $mail->isSMTP();
+      // Send using SMTP
+      //host miễn phí của gmail
+      $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+      $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+      //username gmail của chính bạn
+      $mail->Username   = 'nguyenvietmanhit@gmail.com';                     // SMTP username
+      //password cho ứng dụng, ko phải password của tài khoảng
+//    đăng nhập gmail
+//    tạo mật khẩu ứng dụng tại link:
+// https://myaccount.google.com/ - menu Bảo mật
+      $mail->Password   = 'yichffdzhetottuw';                               // SMTP password
+      $mail->SMTPSecure = \PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+      $mail->Port       = 587;                                    // TCP port to connect to
+
+      //Recipients
+      $mail->setFrom('abc@gmail.com', 'ABC');
+      //setting mail người gửi
+      $mail->addAddress($email, 'Manh');     // Add a recipient
+//    $mail->addAddress('ellen@example.com');               // Name is optional
+//    $mail->addReplyTo('info@example.com', 'Information');
+//    $mail->addCC('cc@example.com');
+//    $mail->addBCC('bcc@example.com');
+
+      // Attachments
+//      $mail->addAttachment('rose.jpeg');         // Add attachments
+//    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+      // Content
+      $mail->isHTML(true);                                  // Set email format to HTML
+      $mail->Subject = 'Tiêu đề mail';
+      $mail->Body    = 'Cảm ơn bạn đã đặt hàng';
+//    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+      $mail->send();
+      echo 'Message has been sent';
+    } catch (Exception $e) {
+      echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
   }
 }
