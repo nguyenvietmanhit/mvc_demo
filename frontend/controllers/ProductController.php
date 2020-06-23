cd047e6c76c9b9010cefcdafb81843a4349f9237
Nguyen Viet Manh <nvmanh@cmc.com.vn>
2020-06-23 17:43:40 +0700

    update

--- frontend/controllers/ProductController.php
+++ frontend/controllers/ProductController.php
@@ -1,8 +1,21 @@
 <?php
 require_once 'controllers/Controller.php';
+require_once 'models/Product.php';
+require_once 'models/Category.php';
 class ProductController extends Controller {
   public function showAll() {
-    $this->content = $this->render('views/products/show_all.php');
+    //get products
+    $product_model = new Product();
+    $products = $product_model->getProductInHomePage();
+
+    //get categories để filter
+    $category_model = new Category();
+    $categories = $category_model->getAll();
+
+    $this->content = $this->render('views/products/show_all.php', [
+      'products' => $products,
+      'categories' => $categories,
+    ]);
     //show danh sách category bên sidebar left
 
     require_once 'views/layouts/main_product.php';
--- frontend/views/products/show_all.php
+++ frontend/views/products/show_all.php
@@ -1,4 +1,3 @@
-
 <div class="product-main">
 
     <div class="container">
@@ -6,25 +5,27 @@
             <div class="main-left col-md-3 col-sm-3 col-xs-12">
                 <h3>Lọc</h3>
                 <form action="" method="GET">
-                    <div class="form-group">
-                        <b>Danh mục</b> <br />
-                        <input type="checkbox" name="category[]" value="0" /> Tivi <br />
-                        <input type="checkbox" name="category[]" value="1" /> Tủ lạnh <br />
-                        <input type="checkbox" name="category[]" value="2" /> Thể thao <br />
-                        <input type="checkbox" name="category[]" value="2" /> Thế giới <br />
-                        <input type="checkbox" name="category[]" value="2" /> Tin tức <br />
-                        <input type="checkbox" name="category[]" value="2" /> Sức khỏe <br />
-                    </div>
+                  <?php if (!empty($categories)): ?>
+                      <div class="form-group">
+                          <b>Danh mục</b> <br/>
+                          <input type="checkbox" name="category[]" value="0"/> Tivi <br/>
+                          <input type="checkbox" name="category[]" value="1"/> Tủ lạnh <br/>
+                          <input type="checkbox" name="category[]" value="2"/> Thể thao <br/>
+                          <input type="checkbox" name="category[]" value="2"/> Thế giới <br/>
+                          <input type="checkbox" name="category[]" value="2"/> Tin tức <br/>
+                          <input type="checkbox" name="category[]" value="2"/> Sức khỏe <br/>
+                      </div>
+                  <?php endif; ?>
 
                     <div class="form-group">
-                        <b>Khoảng giá</b> <br />
-                        <input type="checkbox" name="price[]" value="1" /> Dưới 1tr <br />
-                        <input type="checkbox" name="price[]" value="2" /> Từ 1 - 2tr <br />
-                        <input type="checkbox" name="price[]" value="3" /> Từ 2 - 3tr <br />
-                        <input type="checkbox" name="price[]" value="4" /> Trên hơn 3tr <br />
+                        <b>Khoảng giá</b> <br/>
+                        <input type="checkbox" name="price[]" value="1"/> Dưới 1tr <br/>
+                        <input type="checkbox" name="price[]" value="2"/> Từ 1 - 2tr <br/>
+                        <input type="checkbox" name="price[]" value="3"/> Từ 2 - 3tr <br/>
+                        <input type="checkbox" name="price[]" value="4"/> Trên hơn 3tr <br/>
                     </div>
                     <div class="form-group">
-                        <input type="submit" name="filter" value="Filter" class="btn btn-primary" />
+                        <input type="submit" name="filter" value="Filter" class="btn btn-primary"/>
                     </div>
                 </form>
             </div>
