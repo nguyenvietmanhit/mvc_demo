70db07b871b3482cff9e5b70e779787a95aa8bf4
Nguyen Viet Manh <nvmanh@cmc.com.vn>
2020-06-23 15:13:17 +0700

    update

--- frontend/views/products/show_all.php
+++ frontend/views/products/show_all.php
@@ -1,116 +1,155 @@
-<div class="filter-left">
-  <ul>
-    <li>
-      <a href="#">Ti vi</a>
-      <a href="#">Tủ lạnh</a>
-    </li>
-  </ul>
-</div>
+
 <div class="product-main">
-  <!--Timeline items start -->
-  <div class="timeline-items">
 
-    <!--ITEM-->
-    <div class="timeline-item">
-      <div class="timeline-left">
-        <div class="timeline-left-wrapper">
-          <a href="#" class="timeline-category" data-zebra-tooltip title="Thể thao"><i
-              class="material-icons">&#xE894;</i></a>
-          <span class="timeline-date">3 phút trước</span>
+    <div class="container">
+        <div class="row">
+            <div class="main-left col-md-3 col-sm-3 col-xs-12">
+                <h3>Lọc</h3>
+                <form action="" method="GET">
+                    <div class="form-group">
+                        <b>Danh mục</b> <br />
+                        <input type="checkbox" name="category[]" value="0" /> Tivi <br />
+                        <input type="checkbox" name="category[]" value="1" /> Tủ lạnh <br />
+                        <input type="checkbox" name="category[]" value="2" /> Thể thao <br />
+                        <input type="checkbox" name="category[]" value="2" /> Thế giới <br />
+                        <input type="checkbox" name="category[]" value="2" /> Tin tức <br />
+                        <input type="checkbox" name="category[]" value="2" /> Sức khỏe <br />
+                    </div>
+
+                    <div class="form-group">
+                        <b>Khoảng giá</b> <br />
+                        <input type="checkbox" name="price[]" value="1" /> Dưới 1tr <br />
+                        <input type="checkbox" name="price[]" value="2" /> Từ 1 - 2tr <br />
+                        <input type="checkbox" name="price[]" value="3" /> Từ 2 - 3tr <br />
+                        <input type="checkbox" name="price[]" value="4" /> Trên hơn 3tr <br />
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" name="filter" value="Filter" class="btn btn-primary" />
+                    </div>
+                </form>
+            </div>
+            <div class="main-right col-md-9 col-sm-9 col-xs-12">
+                <h2>Danh sách sản phẩm</h2>
+                <div class="row">
+                    <div class="product-item col-md-4 col-sm-4 col-xs-12">
+                        <a href="chi-tiet/2" class="product-link">
+                            <img src="../backend/assets/uploads/1584528663-1.JPEG" height="150" class="product-image">
+                        </a>
+                        <div class="home-page">
+                            <a href="#" class="timeline-category-name font-arial">Thể thao</a>
+                            <a href="chi-tiet/2" class="product-link">
+                                <h3 class="timeline-post-title">Vợt cầu lông</h3>
+                            </a>
+                            <div class="product-price timeline-post-info">
+                                121đ
+                            </div>
+                            <div class="timeline-post-info">
+                                <a href="them-vao-gio-hang/2" class="product-cart">
+                                    Thêm vào giỏ hàng
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="product-item col-md-4 col-sm-4 col-xs-12">
+                        <a href="chi-tiet/2" class="product-link">
+                            <img src="../backend/assets/uploads/1584528663-1.JPEG" height="150" class="product-image">
+                        </a>
+                        <div class="home-page">
+                            <a href="#" class="timeline-category-name font-arial">Thể thao</a>
+                            <a href="chi-tiet/2" class="product-link">
+                                <h3 class="timeline-post-title">Vợt cầu lông</h3>
+                            </a>
+                            <div class="product-price timeline-post-info">
+                                121đ
+                            </div>
+                            <div class="timeline-post-info">
+                                <a href="them-vao-gio-hang/2" class="product-cart">
+                                    Thêm vào giỏ hàng
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="product-item col-md-4 col-sm-4 col-xs-12">
+                        <a href="chi-tiet/2" class="product-link">
+                            <img src="../backend/assets/uploads/1584528663-1.JPEG" height="150" class="product-image">
+                        </a>
+                        <div class="home-page">
+                            <a href="#" class="timeline-category-name font-arial">Thể thao</a>
+                            <a href="chi-tiet/2" class="product-link">
+                                <h3 class="timeline-post-title">Vợt cầu lông</h3>
+                            </a>
+                            <div class="product-price timeline-post-info">
+                                121đ
+                            </div>
+                            <div class="timeline-post-info">
+                                <a href="them-vao-gio-hang/2" class="product-cart">
+                                    Thêm vào giỏ hàng
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="product-item col-md-4 col-sm-4 col-xs-12">
+                        <a href="chi-tiet/2" class="product-link">
+                            <img src="../backend/assets/uploads/1584528663-1.JPEG" height="150" class="product-image">
+                        </a>
+                        <div class="home-page">
+                            <a href="#" class="timeline-category-name font-arial">Thể thao</a>
+                            <a href="chi-tiet/2" class="product-link">
+                                <h3 class="timeline-post-title">Vợt cầu lông</h3>
+                            </a>
+                            <div class="product-price timeline-post-info">
+                                121đ
+                            </div>
+                            <div class="timeline-post-info">
+                                <a href="them-vao-gio-hang/2" class="product-cart">
+                                    Thêm vào giỏ hàng
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="product-item col-md-4 col-sm-4 col-xs-12">
+                        <a href="chi-tiet/2" class="product-link">
+                            <img src="../backend/assets/uploads/1584528663-1.JPEG" height="150" class="product-image">
+                        </a>
+                        <div class="home-page">
+                            <a href="#" class="timeline-category-name font-arial">Thể thao</a>
+                            <a href="chi-tiet/2" class="product-link">
+                                <h3 class="timeline-post-title">Vợt cầu lông</h3>
+                            </a>
+                            <div class="product-price timeline-post-info">
+                                121đ
+                            </div>
+                            <div class="timeline-post-info">
+                                <a href="them-vao-gio-hang/2" class="product-cart">
+                                    Thêm vào giỏ hàng
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="product-item col-md-4 col-sm-4 col-xs-12">
+                        <a href="chi-tiet/2" class="product-link">
+                            <img src="../backend/assets/uploads/1584528663-1.JPEG" height="150" class="product-image">
+                        </a>
+                        <div class="home-page">
+                            <a href="#" class="timeline-category-name font-arial">Thể thao</a>
+                            <a href="chi-tiet/2" class="product-link">
+                                <h3 class="timeline-post-title">Vợt cầu lông</h3>
+                            </a>
+                            <div class="product-price timeline-post-info">
+                                121đ
+                            </div>
+                            <div class="timeline-post-info">
+                                <a href="them-vao-gio-hang/2" class="product-cart">
+                                    Thêm vào giỏ hàng
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="timeline-right">
-        <div class="timeline-post-image">
-          <a href="#">
-            <img src="http://tevratgundogdu.com/works/ideabox-html-template/img/news-test-images/news-img8.jpg"
-                 width="260">
-          </a>
-        </div>
-        <div class="home-page timeline-post-content">
-          <a href="#" class="timeline-category-name font-arial">Điện thoại</a>
-          <a href="not-defined.html">
-            <h3 class="timeline-post-title">Điện thoại Samsung Galaxy Note 10 Lite</h3>
-          </a>
-          <div class="product-price timeline-post-info">
-            12.490.000₫
-          </div>
-          <div class="timeline-post-info">
-            <a href="#" class="product-cart">
-              Thêm vào giỏ hàng
-            </a>
-          </div>
-        </div>
-      </div>
+
     </div>
-    <!--END ITEM-->
-
-    <!--ITEM-->
-    <div class="timeline-item">
-      <div class="timeline-left">
-        <div class="timeline-left-wrapper">
-          <a href="#" class="timeline-category" data-zebra-tooltip title="Thể thao"><i
-              class="material-icons">&#xE894;</i></a>
-          <span class="timeline-date">3 phút trước</span>
-        </div>
-      </div>
-      <div class="timeline-right">
-        <div class="timeline-post-image">
-          <a href="#">
-            <img src="http://tevratgundogdu.com/works/ideabox-html-template/img/news-test-images/news-img8.jpg"
-                 width="260">
-          </a>
-        </div>
-        <div class="home-page timeline-post-content">
-          <a href="#" class="timeline-category-name font-arial">Điện thoại</a>
-          <a href="not-defined.html">
-            <h3 class="timeline-post-title">Điện thoại Samsung Galaxy Note 10 Lite</h3>
-          </a>
-          <div class="product-price timeline-post-info">
-            12.490.000₫
-          </div>
-          <div class="timeline-post-info">
-            <a href="#" class="product-cart">
-              Thêm vào giỏ hàng
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!--END ITEM-->
-
-    <!--ITEM-->
-    <div class="timeline-item">
-      <div class="timeline-left">
-        <div class="timeline-left-wrapper">
-          <a href="#" class="timeline-category" data-zebra-tooltip title="Thể thao"><i
-              class="material-icons">&#xE894;</i></a>
-          <span class="timeline-date">3 phút trước</span>
-        </div>
-      </div>
-      <div class="timeline-right">
-        <div class="timeline-post-image">
-          <a href="#">
-            <img src="http://tevratgundogdu.com/works/ideabox-html-template/img/news-test-images/news-img8.jpg"
-                 width="260">
-          </a>
-        </div>
-        <div class="home-page timeline-post-content">
-          <a href="#" class="timeline-category-name font-arial">Điện thoại</a>
-          <a href="not-defined.html">
-            <h3 class="timeline-post-title">Điện thoại Samsung Galaxy Note 10 Lite</h3>
-          </a>
-          <div class="product-price timeline-post-info">
-            12.490.000₫
-          </div>
-          <div class="timeline-post-info">
-            <a href="#" class="product-cart">
-              Thêm vào giỏ hàng
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!--END ITEM-->
-
-  </div>
 </div>
+</div>
+
