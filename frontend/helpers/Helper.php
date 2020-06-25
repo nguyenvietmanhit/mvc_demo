f9b1d00f36d9affcf2b92c4038c816d65c074866
Nguyen Viet Manh <nvmanh@cmc.com.vn>
2020-06-25 17:24:26 +0700

    update

--- frontend/helpers/Helper.php
+++ frontend/helpers/Helper.php
@@ -7,28 +7,28 @@
  */
 class Helper
 {
-    const STATUS_ACTIVE = 1;
-    const STATUS_DISABLED = 0;
-    const STATUS_ACTIVE_TEXT = 'Active';
-    const STATUS_DISABLED_TEXT = 'Disabled';
+  const STATUS_ACTIVE = 1;
+  const STATUS_DISABLED = 0;
+  const STATUS_ACTIVE_TEXT = 'Active';
+  const STATUS_DISABLED_TEXT = 'Disabled';
 
-    /**
-     * Get status text
-     * @param int $status
-     * @return string
-     */
-    public static function getStatusText($status = 0) {
-        $status_text = '';
-        switch ($status) {
-            case self::STATUS_ACTIVE:
-                $status_text = self::STATUS_ACTIVE_TEXT;
-                break;
-            case self::STATUS_DISABLED:
-                $status_text = self::STATUS_DISABLED_TEXT;
-                break;
-        }
-        return $status_text;
+  /**
+   * Get status text
+   * @param int $status
+   * @return string
+   */
+  public static function getStatusText($status = 0) {
+    $status_text = '';
+    switch ($status) {
+      case self::STATUS_ACTIVE:
+        $status_text = self::STATUS_ACTIVE_TEXT;
+        break;
+      case self::STATUS_DISABLED:
+        $status_text = self::STATUS_DISABLED_TEXT;
+        break;
     }
+    return $status_text;
+  }
 
   public static function getSlug($str) {
     $str = trim(mb_strtolower($str));
--- frontend/views/payments/index.php
+++ frontend/views/payments/index.php
@@ -1,3 +1,6 @@
+<?php
+require_once 'helpers/Helper.php';
+?>
 <div class="container">
     <form action="" method="POST">
         <div class="row">
@@ -5,19 +8,19 @@
                 <h5 class="center-align">Thông tin khách hàng</h5>
                 <div class="form-group">
                     <label>Họ tên khách hàng</label>
-                    <input type="text" name="fullname" value=""  class="form-control" placeholder="">
+                    <input type="text" name="fullname" value="" class="form-control" placeholder="">
                 </div>
                 <div class="form-group">
                     <label>Địa chỉ</label>
-                    <input type="text" name="address" value=""  class="form-control" placeholder="">
+                    <input type="text" name="address" value="" class="form-control" placeholder="">
                 </div>
                 <div class="form-group">
                     <label>SĐT</label>
-                    <input type="number" min="0" name="mobile" value=""  class="form-control" placeholder="">
+                    <input type="number" min="0" name="mobile" value="" class="form-control" placeholder="">
                 </div>
                 <div class="form-group">
                     <label>Email</label>
-                    <input type="email" min="0" name="email" value=""  class="form-control" placeholder="">
+                    <input type="email" min="0" name="email" value="" class="form-control" placeholder="">
                 </div>
                 <div class="form-group">
                     <label>Ghi chú thêm</label>
@@ -25,51 +28,73 @@
                 </div>
             </div>
             <div class="col-md-6 col-sm-6">
+              <?php
+              echo "<pre>" . __LINE__ . ", " . __DIR__ . "<br />";
+              print_r($_SESSION);
+              echo "</pre>";
+              //                die;
+              ?>
                 <h5 class="center-align">Thông tin đơn hàng của bạn</h5>
-                <table class="table table-bordered">
-                    <tbody>
-                    <tr>
-                        <th width="40%">Tên sản phẩm</th>
-                        <th width="12%">Số lượng</th>
-                        <th>Giá</th>
-                        <th>Thành tiền</th>
-                    </tr>
-                    <tr>
-                        <td>
-                            <img class="product-avatar img-responsive"
-                                 src="../backend/assets/uploads/products-1564468873product2.jpg" width="60">
-                            <div class="content-product">
-                                <a href="san-pham/dien-thoai-oppo-f11-pro-128gb/2" class="content-product-a">
-                                    Điện thoại OPPO F11 Pro 128GB </a>
-                            </div>
-                        </td>
-                        <td>
-                                              <span class="product-amount">
-                                                  1                                              </span>
-                        </td>
-                        <td>
-                                              <span class="product-price-payment">
-                                                 8.490.000 vnđ
-                                              </span>
-                        </td>
-                        <td>
-                                              <span class="product-price-payment">
-                                                 8.490.000  vnđ
-                                              </span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colspan="5" class="product-total">
-                            Tổng giá trị đơn hàng:
-                            <span class="product-price">
-                                                8.490.000 vnđ
-                                            </span>
-                        </td>
-                    </tr>
-
-                    </tbody>
-                </table>
-
+              <?php
+              //biến lưu tổng giá trị đơn hàng
+              $total = 0;
+              if (isset($_SESSION['cart'])):
+                ?>
+                  <table class="table table-bordered">
+                      <tbody>
+                      <tr>
+                          <th width="40%">Tên sản phẩm</th>
+                          <th width="12%">Số lượng</th>
+                          <th>Giá</th>
+                          <th>Thành tiền</th>
+                      </tr>
+                      <?php foreach ($_SESSION['cart'] AS $product_id => $cart):
+                        $product_link = 'san-pham/' . Helper::getSlug($cart['name']) . "/$product_id";
+                        ?>
+                          <tr>
+                              <td>
+                                <?php if (!empty($cart['avatar'])): ?>
+                                    <img class="product-avatar img-responsive"
+                                         src="../backend/assets/uploads/<?php echo $cart['avatar']; ?>" width="60"/>
+                                <?php endif; ?>
+                                  <div class="content-product">
+                                      <a href="<?php echo $product_link; ?>" class="content-product-a">
+                                        <?php echo $cart['name']; ?>
+                                      </a>
+                                  </div>
+                              </td>
+                              <td>
+                              <span class="product-amount">
+                                  <?php echo $cart['quality']; ?>
+                              </span>
+                              </td>
+                              <td>
+                              <span class="product-price-payment">
+                                 <?php echo number_format($cart['price'], 0, '.', '.') ?> vnđ
+                              </span>
+                              </td>
+                              <td>
+                              <span class="product-price-payment">
+                                  <?php
+                                  $price_total = $cart['price'] * $cart['quality'];
+                                  $total += $price_total;
+                                  ?>
+                                <?php echo number_format($price_total, 0, '.', '.') ?> vnđ
+                              </span>
+                              </td>
+                          </tr>
+                      <?php endforeach; ?>
+                      <tr>
+                          <td colspan="5" class="product-total">
+                              Tổng giá trị đơn hàng:
+                              <span class="product-price">
+                                <?php echo number_format($total, 0, '.', '.') ?> vnđ
+                            </span>
+                          </td>
+                      </tr>
+                      </tbody>
+                  </table>
+              <?php endif; ?>
                 <input type="submit" name="submit" value="Thanh toán" class="btn btn-primary">
                 <a href="gio-hang-cua-ban" class="btn btn-secondary">Về trang giỏ hàng</a>
             </div>
