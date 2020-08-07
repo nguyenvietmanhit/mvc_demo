a294a88f5be7b501d2803090d40a8f13dbb2e42b
Nguyen Viet Manh <nvmanh@cmc.com.vn>
2020-08-07 10:01:53 +0700

    update

--- backend/configs/database.php
+++ backend/configs/database.php
@@ -1,4 +1,10 @@
 <?php
-const DB_DSN = 'mysql:host=localhost;dbname=mvc_demo;charset=utf8';
+const DB_DSN = 'mysql:host=localhost;dbname=mvc_proj;charset=utf8';
 const DB_USERNAME = 'root';
 const DB_PASSWORD = '';
+
+class Database {
+  const DB_DSN = 'mysql:host=localhost;dbname=mvc_proj;charset=utf8';
+  const DB_USERNAME = 'root';
+  const DB_PASSWORD = '';
+}
