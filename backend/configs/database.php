811646708d772715e8927ba90521ebd21ecfdd2d
Manh <nvmanh@cmc.com.vn>
2020-08-16 22:57:07 +0700

    a

--- backend/configs/database.php
+++ backend/configs/database.php
@@ -1,8 +1,4 @@
 <?php
-const DB_DSN = 'mysql:host=localhost;dbname=mvc_proj;charset=utf8';
-const DB_USERNAME = 'root';
-const DB_PASSWORD = '';
-
 class Database {
   const DB_DSN = 'mysql:host=localhost;dbname=mvc_demo;charset=utf8';
   const DB_USERNAME = 'root';
