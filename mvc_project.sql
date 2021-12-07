b635a01f8e48b1c684b0e7430361a7b49a98dc77
Nguyen Viet Manh <nguyenvietmanhit@gmail.com>
2021-12-07 11:17:13 +0700

    a

--- mvc_project.sql
+++ mvc_project.sql
@@ -1,47 +1,51 @@
-/*
-Navicat MySQL Data Transfer
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 07, 2021 at 05:15 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.4.24
 
-Source Server         : localhost
-Source Server Version : 50505
-Source Host           : localhost:3306
-Source Database       : mvc_project
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-Target Server Type    : MYSQL
-Target Server Version : 50505
-File Encoding         : 65001
 
-Date: 2021-05-03 12:31:53
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-SET FOREIGN_KEY_CHECKS=0;
+--
+-- Database: `mvc_project`
+--
 
--- ----------------------------
--- Table structure for categories
--- ----------------------------
-DROP TABLE IF EXISTS `categories`;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
 CREATE TABLE `categories` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL COMMENT 'Tên danh mục',
   `type` tinyint(3) DEFAULT 0 COMMENT 'Loại danh mục: 0 - Product, 1 - News',
   `avatar` varchar(255) DEFAULT NULL COMMENT 'Tên file ảnh danh mục',
   `description` text DEFAULT NULL COMMENT 'Mô tả chi tiết cho danh mục',
   `status` tinyint(3) DEFAULT 0 COMMENT 'Trạng thái danh mục: 0 - Inactive, 1 - Active',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'Ngày tạo danh mục',
-  `updated_at` datetime DEFAULT NULL COMMENT 'Ngày cập nhật cuối',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+  `updated_at` datetime DEFAULT NULL COMMENT 'Ngày cập nhật cuối'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- ----------------------------
--- Records of categories
--- ----------------------------
-INSERT INTO `categories` VALUES ('1', 'dsadsa', '0', '', 'ds', '0', '2021-05-03 12:30:55', null);
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table structure for news
--- ----------------------------
-DROP TABLE IF EXISTS `news`;
+--
+-- Table structure for table `news`
+--
+
 CREATE TABLE `news` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `category_id` int(11) DEFAULT NULL COMMENT 'Id của danh mục mà tin tức thuộc về, là khóa ngoại liên kết với bảng categories',
   `title` varchar(255) NOT NULL COMMENT 'Tiêu đề tin tức',
   `summary` varchar(255) DEFAULT NULL COMMENT 'Mô tả ngắn cho tin tức',
@@ -52,22 +56,17 @@ CREATE TABLE `news` (
   `seo_description` varchar(255) DEFAULT NULL COMMENT 'Từ khóa seo cho phần mô tả',
   `seo_keywords` varchar(255) DEFAULT NULL COMMENT 'Các từ khóa seo',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'Ngày tạo',
-  `updated_at` datetime DEFAULT NULL COMMENT 'Ngày cập nhật cuối',
-  PRIMARY KEY (`id`) USING BTREE,
-  KEY `category_id` (`category_id`) USING BTREE,
-  CONSTRAINT `news_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+  `updated_at` datetime DEFAULT NULL COMMENT 'Ngày cập nhật cuối'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- ----------------------------
--- Records of news
--- ----------------------------
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table structure for orders
--- ----------------------------
-DROP TABLE IF EXISTS `orders`;
+--
+-- Table structure for table `orders`
+--
+
 CREATE TABLE `orders` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `user_id` int(11) DEFAULT NULL COMMENT 'Id của user trong trường hợp đã login và đặt hàng, là khóa ngoại liên kết với bảng users',
   `fullname` varchar(255) DEFAULT NULL COMMENT 'Tên khách hàng',
   `address` varchar(255) DEFAULT NULL COMMENT 'Địa chỉ khách hàng',
@@ -77,39 +76,30 @@ CREATE TABLE `orders` (
   `price_total` int(11) DEFAULT NULL COMMENT 'Tổng giá trị đơn hàng',
   `payment_status` tinyint(2) DEFAULT NULL COMMENT 'Trạng thái đơn hàng: 0 - Chưa thành toán, 1 - Đã thành toán',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'Ngày tạo đơn',
-  `updated_at` datetime DEFAULT NULL COMMENT 'Ngày cập nhật cuối',
-  PRIMARY KEY (`id`) USING BTREE,
-  KEY `user_id` (`user_id`) USING BTREE,
-  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+  `updated_at` datetime DEFAULT NULL COMMENT 'Ngày cập nhật cuối'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- ----------------------------
--- Records of orders
--- ----------------------------
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table structure for order_details
--- ----------------------------
-DROP TABLE IF EXISTS `order_details`;
+--
+-- Table structure for table `order_details`
+--
+
 CREATE TABLE `order_details` (
   `order_id` int(11) DEFAULT NULL COMMENT 'Id của order tương ứng, là khóa ngoại liên kết với bảng orders',
   `product_name` varchar(150) DEFAULT NULL COMMENT 'Tên sp tại thời điểm đặt hàng',
   `product_price` int(11) DEFAULT NULL COMMENT 'Giá sản phẩm tương ứng tại thời điểm đặt hàng',
-  `quantity` int(11) DEFAULT NULL COMMENT 'Số lượng sản phẩm tương ứng tại thời điểm đặt hàng',
-  KEY `order_id` (`order_id`) USING BTREE,
-  CONSTRAINT `order_details_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+  `quantity` int(11) DEFAULT NULL COMMENT 'Số lượng sản phẩm tương ứng tại thời điểm đặt hàng'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- ----------------------------
--- Records of order_details
--- ----------------------------
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table structure for products
--- ----------------------------
-DROP TABLE IF EXISTS `products`;
+--
+-- Table structure for table `products`
+--
+
 CREATE TABLE `products` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `category_id` int(11) DEFAULT NULL COMMENT 'Id của danh mục mà sản phẩm thuộc về, là khóa ngoại liên kết với bảng categories',
   `title` varchar(255) DEFAULT NULL COMMENT 'Tên sản phẩm',
   `avatar` varchar(255) DEFAULT NULL COMMENT 'Tên file ảnh sản phẩm',
@@ -122,23 +112,17 @@ CREATE TABLE `products` (
   `seo_description` varchar(255) DEFAULT NULL COMMENT 'Từ khóa seo cho phần mô tả',
   `seo_keywords` varchar(255) DEFAULT NULL COMMENT 'Các từ khóa seo',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'Ngày tạo',
-  `updated_at` datetime DEFAULT NULL COMMENT 'Ngày cập nhật cuối',
-  PRIMARY KEY (`id`) USING BTREE,
-  KEY `category_id` (`category_id`) USING BTREE,
-  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+  `updated_at` datetime DEFAULT NULL COMMENT 'Ngày cập nhật cuối'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- ----------------------------
--- Records of products
--- ----------------------------
-INSERT INTO `products` VALUES ('1', '1', 'Merge request #2555', '', '1', '2', '', '', '1', '', '', '', '2021-05-03 12:31:05', null);
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table structure for users
--- ----------------------------
-DROP TABLE IF EXISTS `users`;
+--
+-- Table structure for table `users`
+--
+
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `username` varchar(255) DEFAULT NULL COMMENT 'Tên đăng nhập',
   `password` varchar(255) DEFAULT NULL COMMENT 'Mật khẩu đăng nhập',
   `first_name` varchar(255) DEFAULT NULL COMMENT 'Fist name',
@@ -152,11 +136,115 @@ CREATE TABLE `users` (
   `facebook` varchar(255) DEFAULT NULL COMMENT 'Link facebook',
   `status` tinyint(3) DEFAULT 0 COMMENT 'Trạng thái danh mục: 0 - Inactive, 1 - Active',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'Ngày tạo',
-  `updated_at` datetime DEFAULT NULL COMMENT 'Ngày cập nhật cuối',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+  `updated_at` datetime DEFAULT NULL COMMENT 'Ngày cập nhật cuối'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- ----------------------------
--- Records of users
--- ----------------------------
-INSERT INTO `users` VALUES ('1', 'admin', 'c4ca4238a0b923820dcc509a6f75849b', null, null, null, null, null, null, null, null, null, '1', '2021-05-03 12:29:18', null);
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `order_details`
+--
+ALTER TABLE `order_details`
+  ADD KEY `order_id` (`order_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `news`
+--
+ALTER TABLE `news`
+  ADD CONSTRAINT `news_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `order_details`
+--
+ALTER TABLE `order_details`
+  ADD CONSTRAINT `order_details_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`);
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
