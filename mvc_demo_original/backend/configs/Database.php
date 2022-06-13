e3e090947ab51ae4fdce6f016a9a74193cde5e98
Nguyen Viet Manh <nguyenvietmanhit@gmail.com>
2022-06-13 12:00:42 +0700

    A

--- mvc_demo_original/backend/configs/Database.php
+++ mvc_demo_original/backend/configs/Database.php
@@ -1,4 +1,8 @@
 <?php
-//Tên class sẽ trùng với tên file
-class Database {
+
+class Database
+{
+    const DB_DSN = 'mysql:host=localhost;dbname=;port=3306;charset=utf8';
+    const DB_USERNAME = 'root';
+    const DB_PASSWORD = '';
 }
