92eee35d4056b7087a4020b11708edb435f2c455
Nguyen Viet Manh <nguyenvietmanhit@gmail.com>
2023-04-24 09:59:40 +0700

    a

--- mvc_demo_original_frontend/backend/views/layouts/main.php
+++ mvc_demo_original_frontend/backend/views/layouts/main.php
@@ -29,6 +29,29 @@
     <div class="content-wrapper">
         <!-- Main content -->
         <section class="content">
+            <!--            Hiển thị thông báo lỗi và thành công-->
+            <?php if (!empty($this->error)): ?>
+                <div class="alert alert-danger"><?php echo $this->error ?></div>
+            <?php endif; ?>
+    
+            <?php if (isset($_SESSION['error'])): ?>
+                <div class="alert alert-danger">
+                    <?php
+                    echo $_SESSION['error'];
+                    unset($_SESSION['error']);
+                    ?>
+                </div>
+            <?php endif; ?>
+    
+            <?php if (isset($_SESSION['success'])): ?>
+                <div class="alert alert-success">
+                    <?php
+                    echo $_SESSION['success'];
+                    unset($_SESSION['success']);
+                    ?>
+                </div>
+            <?php endif; ?>
+            
 <!--            Nội dung hiển thị ở đây-->
             <?php echo $this->content; ?>
         </section>
