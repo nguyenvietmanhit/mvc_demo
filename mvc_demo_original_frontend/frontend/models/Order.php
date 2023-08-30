e353c5f698a61e46f4b340b7929cd12e13e7ca33
Nguyen Viet Manh <nguyenvietmanhit@gmail.com>
2023-08-30 09:12:01 +0700

    a

--- mvc_demo_original_frontend/frontend/models/Order.php
+++ mvc_demo_original_frontend/frontend/models/Order.php
@@ -1,5 +1,37 @@
 <?php
 require_once 'models/Model.php';
 class Order extends Model {
+	
+	public function insertOrder($fullname, $address, $mobile, $email, $note, $price_total, $payment_status) {
+		$sql_insert = "INSERT INTO orders(fullname, address, mobile, email, note, price_total, payment_status)
+    VALUES (:fullname, :address, :mobile, :email, :note, :price_total, :payment_status)";
+		$obj_insert = $connection->prepare($sql_insert);
+		$arr_insert = [
+			':fullname' => $fullname,
+			':address' => $address,
+			':mobile' => $mobile,
+			':email' => $email,
+			':note' => $note,
+			':price_total' => $price_total,
+			':payment_status' => $payment_status,
+		];
+		$obj_insert->execute($arr_insert);
+		// Trả về id của order vừa mới insert thành công
+		$order_id = $this->connection->lastInsertId();
+		
+		return $order_id;
+//    return $obj_insert->execute($arr_insert);
+	}
+	
+	public function updatePaymentStatus($id) {
+		$sql_update = "UPDATE orders SET payment_status=:payment_status WHERE id=:id";
 
+		$obj_update = $this->connection->prepare($sql_update);
+		$updates = [
+			':payment_status' => 1, // 1 = Đã thanh toán
+			':id' => $id
+		];
+		return $obj_update->execute($updates);
+	}
+	
 }
